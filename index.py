3443e42f5e2ecec7df8bb0b8269e557e9001bc22
Angel Racini <angelricardoracinimeza@gmail.com>
2021-02-09 10:43:04 -0500

    first

---
+++ index.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('Pagina_web.html')
+
+@app.route('/Formulario.html')
+def formulario():
+    return render_template('Formulario.html')
+
+@app.route('/Taller_web.html')
+def tallerweb():
+    return render_template('Taller_web.html')    
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    #host="0.0.0.0", port = "443"
