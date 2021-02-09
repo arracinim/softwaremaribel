6f316424aa9e4d11d56feda69c9d1dfd5b127b52
Angel Racini <47869936+arracinim@users.noreply.github.com>
2021-02-09 11:46:31 -0500

    Update index.py

--- index.py
+++ index.py
@@ -17,4 +17,4 @@ def tallerweb():
     return render_template('Taller_web.html')    
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=8080, debug=True)
+    app.run(host='0.0.0.0', port=443, debug=True)
